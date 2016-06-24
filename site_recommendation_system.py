13fc9fd15fe0f987f528b3f070b831c4baa36217
Jeffrey Lin <jeffrey82221@gmail.com>
2016-06-24 16:38:44 +0800

    fix

--- site_recommendation_system.py
+++ site_recommendation_system.py
@@ -1,50 +1,49 @@
 # @REVIEW : Read in data
 
-# REVIEW:Read in the crawed wiki pages
-import pickle
-import six.moves.cPickle as pickle
-# load data
-with open("pages.dat", 'rb') as f:
-    wiki_pages = pickle.load(f)
+def similar_site_lists(input,k):
+    #k = 10
+    #input = '中正紀念堂'
 
-# REVIEW:Read in the site names and site data
-import pandas as pd
-sites_data = pd.read_excel('sitedata.xlsx', 'Section',
-                           index_col='Section_Id', na_values=['NA'])
+    # REVIEW:Read in the crawed wiki pages
+    import pickle
+    import six.moves.cPickle as pickle
+    # load data
+    with open("pages.dat", 'rb') as f:
+        wiki_pages = pickle.load(f)
 
-
-wiki_links = []
-for title in sites_data[['stitle']].values:
-    try:
-        string = HanziConv.toTraditional("。".join(merge_lists(
-            [attribute['links'] for attribute in wiki_pages[title[0]].values()])))
-        wiki_links.append(string)
-    except:
-        wiki_links.append("。")
-
-prior_table, condi_table, doc_cov_table, class_cov_table, doc_vec_table, w_doc_vec_table = analysis(
-    pd, sites_data[['stitle']], sites_data[['stitle']], pd.DataFrame(wiki_links), 0.,0.)
+    # REVIEW:Read in the site names and site data
+    import pandas as pd
+    sites_data = pd.read_excel('sitedata.xlsx', 'Section',
+                               index_col='Section_Id', na_values=['NA'])
 
 
+    wiki_links = []
+    for title in sites_data[['stitle']].values:
+        try:
+            string = HanziConv.toTraditional("。".join(merge_lists(
+                [attribute['links'] for attribute in wiki_pages[title[0]].values()])))
+            wiki_links.append(string)
+        except:
+            wiki_links.append("。")
 
-
-# NOTE: the best space :
-import numpy as np
-np.shape(k_nearest_neighbor_with_distances(doc_cov_table.transpose(), 318)[1])
-np.shape(k_nearest_neighbor_with_distances(doc_cov_table.transpose(), 318)[0])
-
-# NOTE: the second best space :
-ct_wiki_summary = condi_table.transpose().sort_index()
-
-dc_wiki_link = doc_cov_table.transpose().sort_index()
-pd.concat([ct_wiki_summary, dc_wiki_link], axis=1)
-k = 10
-
-ranking_lists,score_matrix = k_nearest_neighbor_with_distances(pd.concat([ct_wiki_summary, dc_wiki_link], axis=1), k)
+    prior_table, condi_table, doc_cov_table, class_cov_table, doc_vec_table, w_doc_vec_table = analysis(
+        pd, sites_data[['stitle']], sites_data[['stitle']], pd.DataFrame(wiki_links), 0.,0.)
 
 
 
-input = '中正紀念堂'
-index = list(ranking_lists.index).index(input)
-np.array(ranking_lists)[index,:]
-score_matrix[index,:]
+    import numpy as np
+
+    # NOTE: the best space :
+    dc_wiki_link = doc_cov_table.transpose().sort_index()
+
+    # NOTE: the second best space :
+    ct_wiki_summary = condi_table.transpose().sort_index()
+
+
+    ranking_lists,score_matrix = k_nearest_neighbor_with_distances(pd.concat([ct_wiki_summary, dc_wiki_link], axis=1), k)
+
+    index = list(ranking_lists.index).index(input)
+    return np.array(ranking_lists)[index,:],score_matrix[index,:]
+
+
+similar_site_lists('中正紀念堂',100)
