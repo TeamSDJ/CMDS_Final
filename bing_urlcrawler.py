c63139793f58055e3867722ba3ffaf63032e9785
Jeffrey Lin <jeffrey82221@gmail.com>
2016-06-24 08:53:29 +0800

    reset key

--- bing_urlcrawler.py
+++ bing_urlcrawler.py
@@ -5,31 +5,35 @@ import six.moves.cPickle as pickle
 with open("taiwan_sites.dat", 'rb') as f:
     wiki_pages = pickle.load(f)
 
+
+
+
+
+
+
 site_names = list(wiki_pages.keys())
 len(site_names)
 
 pages_dict = dict()
 from py_bing_search import PyBingWebSearch
 count = 0
-for site_name in site_names:
+for site_name in site_names[0:10]:
     count = count + 1
     print(count,site_name)
     # hkxX68ZPyntLT6qdbLnlbk6NqXCv5fQadYm0BGSUER8
     bing_web = PyBingWebSearch(
-        'hkxX68ZPyntLT6qdbLnlbk6NqXCv5fQadYm0BGSUER8','"'+site_name+'"'+' 痞客邦 pixnet')
+        'PtmTrhdWTdbSZTpKw/dfN2dc3TL4DsC1QGOWHsW41yA','"'+site_name+'"'+' 痞客邦 pixnet')
     pages = bing_web.search(limit=50, format='json')
-    pages_dict[site_name]=pages
+    page_list = []
+    for page in pages:
+        page_list.append({"url":page.url,"title":page.title,"description":page.description})
+    pages_dict[site_name]=page_list
 
-url_dict = dict()
-for key in pages_dict.keys():
-    pages = []
-    for page in pages_dict[key]:
-        pages.append({"url":page.url,"title":page.title,"description":page.description})
-    url_dict[key]=pages
+
 
 # REVIEW: store the crawled wiki pages as a serialized object in page.dat
 import six.moves.cPickle as pickle
 
 # save data
 with open("taiwan_sites_page.dat", "wb") as f:
-    pickle.dump(url_dict, f, protocol=1)
+    pickle.dump(pages_dict, f, protocol=1)
